461bf78176c284c28bc71b97c40c19e6754ea0f1
Louis <huynguyennbk@gmail.com>
2023-07-15 19:10:18 +0700

    setup: orchestrator service nestjs app

---
+++ services/orchestrator/src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { RequestMethod } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api', {
+    exclude: [{ path: 'health', method: RequestMethod.GET }],
+  });
+  await app.listen(3000);
+}
+bootstrap();
---
+++ services/orchestrator/src/typeorm.config.ts
@@ -0,0 +1,21 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST || 'localhost',
+  port: +process.env.POSTGRES_PORT || 5432,
+  username: process.env.POSTGRES_USER || 'admin',
+  password: process.env.POSTGRES_PASSWORD || '123456',
+  database: process.env.POSTGRES_DB || 'orchestrator-svc',
+  entities: [__dirname + '/**/*.entity.ts', __dirname + '/**/*.entity.js'],
+  migrationsRun: false,
+  logging: true,
+  migrationsTableName: 'migration',
+  migrations: [
+    __dirname + '/migration/**/*.ts',
+    __dirname + '/migration/**/*.js',
+  ],
+  synchronize: false,
+};
+
+export = typeOrmConfig;
