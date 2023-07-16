b4a65623769cb0c3ea237bbdb5a8964be6f85d10
Louis <huynguyennbk@gmail.com>
2023-07-16 14:10:25 +0700

    refactor: collections rest should be correct port

--- services/auth/src/app.controller.ts
+++ services/auth/src/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
-  @Get('/healthy')
+  @Get('/health')
   @HttpCode(200)
   getHello(): string {
     return this.appService.healthy();
