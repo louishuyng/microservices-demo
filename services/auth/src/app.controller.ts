50b39cf02a3efe0b479f4e3e36e47f6400ecddcf
Louis <huynguyennbk@gmail.com>
2023-07-16 14:09:26 +0700

    feat: setup healthy api for auth service

--- services/auth/src/app.controller.ts
+++ services/auth/src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
-  @Get()
+  @Get('/healthy')
+  @HttpCode(200)
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.healthy();
   }
 }
